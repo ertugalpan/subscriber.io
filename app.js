5ca65c3602e60c86e051a12c7d24998b3df6a90c
ertugalpan <90673972+ertugalpan@users.noreply.github.com>
2023-01-25 19:16:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mailchimp.setConfig({
+  apiKey: "8670baaa4a741d3f5be4b568094bd66d",
+  server: "us11",
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const listId = "f41c08a2e8";
+  const subscribingUser = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+  };
+
+  async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+
+    if (response.status === "failure") {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      res.sendFile(__dirname + "/success.html");
+    }
+  }
+
+  run();
+});
+
+app.post("/success.html", function (req, res) {
+  res.redirect("/");
+});
+
+app.post("/failure.html", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server is listening in port 3000");
+});
